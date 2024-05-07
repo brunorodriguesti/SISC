b4bf45061d828f711128e83381967c45d0c25b87
brunorodriguesti <brunorodriguesti@yahoo.com.br>
2024-05-06 21:25:57 -0300

    initialize

---
+++ api/src/main/java/com/ltd/sisc/SiscApplication.java
@@ -0,0 +1,13 @@
+package com.ltd.sisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ltd/sisc/SiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltd.sisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
