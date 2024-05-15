fe1c7d7f7cd2b9a2722ab60d39577a02f993e3fc
RicCost4 <richardarcosta@hotmail.com>
2024-05-15 17:23:35 -0300

    implementando metodo de ativas as perguntas ao inserir o nome e o cpf

--- tela/src/app/inscricao/candidato/candidato.component.ts
+++ tela/src/app/inscricao/candidato/candidato.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-candidato',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule],
   templateUrl: './candidato.component.html',
   styleUrl: './candidato.component.scss'
 })
 export class CandidatoComponent {
+  @Output() ativarPerguntas: EventEmitter<any> = new EventEmitter();
+  nome: string = "";
+  cpf: string = "";
 
+  handleClick(): void {
+    if(this.nome != "" && this.cpf != ""){
+      this.ativarPerguntas.emit();
+    }
+  }
 }
--- tela/src/app/inscricao/inscricao.component.ts
+++ tela/src/app/inscricao/inscricao.component.ts
@@ -10,5 +10,9 @@ import { PerguntaComponent } from './pergunta/pergunta.component'
   styleUrl: './inscricao.component.scss'
 })
 export class InscricaoComponent {
-
+  seccaoPerguntas: boolean = false
+  
+  onAtivarPerguntas() {
+    this.seccaoPerguntas = true
+  }
 }
