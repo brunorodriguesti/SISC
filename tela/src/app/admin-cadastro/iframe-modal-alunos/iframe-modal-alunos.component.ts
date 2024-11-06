66cdc8aa6aa75dc98006ca7aa3ee46aee0c539d0
RicCost4 <richard.ar.costa@outlook.com.br>
2024-11-06 19:32:52 -0300

    Fea: tela de visualização dos alunos e formatando data e hora

---
+++ tela/src/app/admin-cadastro/iframe-modal-alunos/iframe-modal-alunos.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { objPessoaId } from '../../DTO';
+
+@Component({
+  selector: 'app-iframe-modal-alunos',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    FormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+  ],
+  providers: [provideNgxMask()],
+  templateUrl: './iframe-modal-alunos.component.html',
+  styleUrl: './iframe-modal-alunos.component.scss'
+})
+export class IframeModalAlunosComponent implements OnInit {
+  objPessoaId: objPessoaId[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<IframeModalAlunosComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.objPessoaId = data.listaAlunos;
+  }
+
+  ngOnInit(): void {
+    this.objPessoaId
+  }
+
+  fecharModal(): void {
+    // Fecha o modal quando o botão de fechar for clicado
+    this.dialogRef.close();
+  }
+}
